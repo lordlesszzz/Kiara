e34159c17d3e9b287b615b5d1bce05929fcb6737
LordlessCamelot <3154536753@qq.com>
2022-05-20 23:04:15 +0800

    Update jd_jinli.ts

--- jd_jinli.ts
+++ jd_jinli.ts
@@ -80,7 +80,7 @@ let min: number[] = [0.02, 0.12, 0.3, 0.6, 0.7, 0.8, 1, 2], log: string = '', lo
   }
 
   for (let [index, value] of cookiesArr.entries()) {
-	if(index < 7){	
+	if(index < 3){	
 	try {
       cookie = value
       UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
