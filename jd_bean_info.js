ad8c9b182988b5411e071dd07663a7449ecacd47
LordlessCamelot <3154536753@qq.com>
2022-05-20 16:59:37 +0800

    Update jd_bean_info.js

--- jd_bean_info.js
+++ jd_bean_info.js
@@ -1,6 +1,5 @@
 /*
  * 详细版京东京豆统计
- * 搬自ccwav
  * 默认不发送通知。
  
 [task_local]
@@ -8,11 +7,12 @@
 20 22 * * * jd_bean_info.js, tag=京豆详情统计, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
  * */
 const $ = new Env('京豆详情统计');
+const notify = $.isNode() ? require('./sendNotify') : '';
+//Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let allMessage = '';
 let myMap = new Map();
 let allBean = 0;
-const {getEnvByPtPin} = require('./function/ql');
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 if ($.isNode()) {
@@ -23,112 +23,65 @@ if ($.isNode()) {
 } else {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
-
-let intcheckckseq=999999;
-let strcheckck = process.env.BOTCHECKCODE;
-let lnShowTop = 0;
-if(!strcheckck){
-	console.log("【账号🆔】没有获取到要查询的账号");
-	return
-}
-if ($.isNode() && process.env.BOTShowTopNum) {
-	lnShowTop = parseInt(process.env.BOTShowTopNum);	
-}
-
-
-for (i = 0; i < cookiesArr.length; i++) {
+!(async () => {
+  if (!cookiesArr[0]) {
+    $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
+    return;
+  }
+  for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
-        cookie = cookiesArr[i];
-        $.pt_pin = (cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-        if (strcheckck == $.pt_pin) {
-            intcheckckseq = i;
-            break;
-        }
-    }
-}
+      cookie = cookiesArr[i];
+      $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
+      $.index = i + 1;
+      $.beanCount = 0;
+      $.incomeBean = 0;
+      $.expenseBean = 0;
+      $.todayIncomeBean = 0;
+      $.errorMsg = '';
+      $.isLogin = true;
+      $.nickName = '';
+      $.message = '';
+      $.balance = 0;
+      $.expiredBalance = 0;
+      await TotalBean();
+      console.log(`\n********开始【京东账号${$.index}】${$.nickName || $.UserName}******\n`);
+      if (!$.isLogin) {
+        $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
 
-if (intcheckckseq == 999999) {
-    if (IsNumber(strcheckck)) {
-        if (parseInt(strcheckck) > cookiesArr.length) {
-            console.log('【账号'+strcheckck+'🆔】你哪来那么多账号,没点逼数吗');
-            return
+        if ($.isNode()) {
+          await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
         }
-        intcheckckseq = parseInt(strcheckck) - 1;
-    }
-}
-console.log("当前查询的CK序号是:"+(intcheckckseq+1));
-
-!(async() => {
-    if (!cookiesArr[intcheckckseq]) {
-        $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {
-            "open-url": "https://bean.m.jd.com/bean/signIndex.action"
-        });
-        return;
-    }
-    i = intcheckckseq;
-	
-    if (cookiesArr[i]) {
-        cookie = cookiesArr[i];
-        $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-        $.index = i + 1;
-        $.beanCount = 0;
-        $.incomeBean = 0;
-        $.expenseBean = 0;
-        $.todayIncomeBean = 0;
-        $.errorMsg = '';
-        $.isLogin = true;
-        $.nickName = '';
-        $.message = '';
-        $.balance = 0;
-        $.expiredBalance = 0;
-        await TotalBean();
-        //console.log(`\n********开始【京东账号${$.index}】${$.nickName || $.UserName}******\n`);
-        if (!$.isLogin) {
-            console.log(`【提示】cookie已失效,\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`);
-            return;			
-        }
-        await bean();
-        await showMsg();
+        continue
+      }
+      await bean();
+      await showMsg();
 
     }
-	
-	var temptest = await getEnvByPtPin($.UserName);
-	var strRemark=getRemark(temptest.remarks);
-	if(strRemark)
-		allMessage = `【账号${(intcheckckseq+1)}详情统计】${strRemark} 收入：${$.todayIncomeBean}京豆\n` +allMessage;
-	else
-		allMessage = `【账号${(intcheckckseq+1)}详情统计】收入：${$.todayIncomeBean}京豆\n` +allMessage;
-	
-	
-    console.log(`${allMessage}`);
-    
+  }
+  allMessage += `\n今日全部账号收入：${allBean}个京豆 🐶\n`
+  if ($.isNode() && allMessage) {
+    await notify.sendNotify(`${$.name}`, `${allMessage}`, { url: `https://bean.m.jd.com/beanDetail/index.action?resourceValue=bean` })
+  }
 })()
-.catch((e) => {
-    // $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
-})
-.finally(() => {
-    $.done();
-})
+    .catch((e) => {
+      // $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
+    })
+    .finally(() => {
+      $.done();
+    })
 async function showMsg() {
   if ($.errorMsg) return
-  
-  var arrayObj=Array.from(myMap);
-  arrayObj.sort(function(a,b){return a[1]-b[1]})
-  if(lnShowTop)
-	  allMessage += "【设定了隐藏" +lnShowTop+"豆以下的信息"+"】 "+'\n'
-  for (var [key, value] of arrayObj) {
-    /* allMessage += key + ' ---> ' +myMap.get(key)+'京豆\n' */
-	if(lnShowTop && lnShowTop>value)
-		continue;
-	allMessage += "【" +value+"豆"+"】 "+key+'\n'
+  allMessage += `\n【账号${$.index}：${$.nickName || $.UserName} 京豆详情统计】\n\n`;
+  allMessage += `今日收入：${$.todayIncomeBean}个京豆 🐶\n`
+  allBean = allBean + parseInt($.todayIncomeBean)
+  for (let key of myMap.keys()) {
+    allMessage += key + ' ---> ' +myMap.get(key)+'京豆 🐶\n'
   }
-}
-function IsNumber(value) {
-    intPerSent = parseInt(value);
-    if (!intPerSent)
-        return false;
-    else
-        return true;
+  myMap = new Map()
+  // if ($.isNode()) {
+  //   await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, `账号${$.index}：${$.nickName || $.UserName}\n昨日收入：${$.incomeBean}京豆 🐶\n昨日支出：${$.expenseBean}京豆 🐶\n当前京豆：${$.beanCount}京豆 🐶${$.message}`, { url: `https://bean.m.jd.com/beanDetail/index.action?resourceValue=bean` })
+  // }
+  // $.msg($.name, '', `账号${$.index}：${$.nickName || $.UserName}\n今日收入：${$.todayIncomeBean}京豆 🐶\n昨日收入：${$.incomeBean}京豆 🐶\n昨日支出：${$.expenseBean}京豆 🐶\n当前京豆：${$.beanCount}(七天将过期${$.expirejingdou})京豆🐶${$.message}`, {"open-url": "https://bean.m.jd.com/beanDetail/index.action?resourceValue=bean", "media-url": "https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png"});
 }
 async function bean() {
   // console.log(`北京时间零点时间戳:${parseInt((Date.now() + 28800000) / 86400000) * 86400000 - 28800000}`);
@@ -139,7 +92,7 @@ async function bean() {
   const tm = parseInt((Date.now() + 28800000) / 86400000) * 86400000 - 28800000 - (24 * 60 * 60 * 1000);
   // 今天0:0:0时间戳
   const tm1 = parseInt((Date.now() + 28800000) / 86400000) * 86400000 - 28800000;
-  let page = 1, t = 0, todayArr = [];
+  let page = 1, t = 0, yesterdayArr = [], todayArr = [];
   do {
     let response = await getJingBeanBalanceDetail(page);
     // console.log(`第${page}页: ${JSON.stringify(response)}`);
@@ -151,7 +104,11 @@ async function bean() {
           const date = item.date.replace(/-/g, '/') + "+08:00";
           if (new Date(date).getTime() >= tm1 && (!item['eventMassage'].includes("退还") && !item['eventMassage'].includes('扣赠'))) {
             todayArr.push(item);
-          } else if (tm > new Date(date).getTime()) {            
+          } else if (tm <= new Date(date).getTime() && new Date(date).getTime() < tm1 && (!item['eventMassage'].includes("退还") && !item['eventMassage'].includes('扣赠'))) {
+            //昨日的
+            yesterdayArr.push(item);
+          } else if (tm > new Date(date).getTime()) {
+            //前天的
             t = 1;
             break;
           }
@@ -170,30 +127,29 @@ async function bean() {
       t = 1;
     }
   } while (t === 0);
-  var strtemp="";
+  for (let item of yesterdayArr) {
+    if (Number(item.amount) > 0) {
+      $.incomeBean += Number(item.amount);
+    } else if (Number(item.amount) < 0) {
+      $.expenseBean += Number(item.amount);
+    }
+  }
   for (let item of todayArr) {
     if (Number(item.amount) > 0) {
       $.todayIncomeBean += Number(item.amount);
-	  strtemp=item.eventMassage;	  
-	  strtemp=strtemp.replace("参加[","").replace("]-奖励","").replace("]店铺活动-奖励","");
-	  strtemp=strtemp.replace("京东自营旗舰店","(自营)").replace("京东自营官方旗舰店","(自营官方)");
-	  strtemp=strtemp.replace("（","(").replace("）",")");
-	  strtemp=strtemp.replace("官方旗舰店","(官方)");
-	  strtemp=strtemp.replace("旗舰店","(旗舰)").replace("专营店","(专营)").replace("专卖店","(专卖)");	  
-      myMap.set(strtemp,0)
+      myMap.set(item.eventMassage,0)
     }
   }
   for (let item of todayArr) {
     if (Number(item.amount) > 0) {
-	  strtemp=item.eventMassage;
-	  strtemp=strtemp.replace("参加[","").replace("]-奖励","").replace("]店铺活动-奖励","");
-	  strtemp=strtemp.replace("京东自营旗舰店","(自营)").replace("京东自营官方旗舰店","(自营官方)");
-	  strtemp=strtemp.replace("（","(").replace("）",")");
-	  strtemp=strtemp.replace("官方旗舰店","(官方)");
-	  strtemp=strtemp.replace("旗舰店","(旗舰)").replace("专营店","(专营)").replace("专卖店","(专卖)");	  
-      myMap.set(strtemp,parseInt(myMap.get(strtemp))+parseInt(item.amount))
+      myMap.set(item.eventMassage,parseInt(myMap.get(item.eventMassage))+parseInt(item.amount))
     }
   }
+  // console.log(myMap)
+  // await queryexpirejingdou();//过期京豆
+  // await redPacket();//过期红包
+  // console.log(`今日收入：${$.todayIncomeBean}个京豆 🐶`);
+  // console.log(`昨日支出：${$.expenseBean}个京豆 🐶`)
 }
 function TotalBean() {
   return new Promise(async resolve => {
@@ -329,21 +285,6 @@ function jsonParse(str) {
       return [];
     }
   }
-}
-
-function getRemark(strRemark) {
-    var strTempRemark = "";
-    if (strRemark) {
-        var Tempindex = strRemark.indexOf("@@");
-        if (Tempindex != -1) {
-			var TempRemarkList = strRemark.split("@@");
-			strTempRemark=TempRemarkList[0];            
-        } else{
-			strTempRemark=strRemark;
-		}
-    }
-   
-    return strTempRemark;
 }
 // prettier-ignore
 function Env(t, e) { "undefined" != typeof process && JSON.stringify(process.env).indexOf("GITHUB") > -1 && process.exit(0); class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise((e, i) => { s.call(this, t, (t, s, r) => { t ? i(t) : e(s) }) }) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `🔔${this.name}, 开始!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } isSurge() { return "undefined" != typeof $httpClient && "undefined" == typeof $loon } isLoon() { return "undefined" != typeof $loon } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; const i = this.getdata(t); if (i) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise(e => { this.get({ url: t }, (t, s, i) => e(i)) }) } runScript(t, e) { return new Promise(s => { let i = this.getdata("@chavy_boxjs_userCfgs.httpapi"); i = i ? i.replace(/\n/g, "").trim() : i; let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r; const [o, h] = i.split("@"), n = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, Accept: "*/*" } }; this.post(n, (t, e, i) => s(i)) }).catch(t => this.logErr(t)) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e); if (!s && !i) return {}; { const i = s ? t : e; try { return JSON.parse(this.fs.readFileSync(i)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r) } } lodash_get(t, e, s) { const i = e.replace(/\[(\d+)\]/g, ".$1").split("."); let r = t; for (const t of i) if (r = Object(r)[t], void 0 === r) return s; return r } lodash_set(t, e, s) { return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t) } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : ""; if (r) try { const t = JSON.parse(r); e = t ? this.lodash_get(t, i, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}"; try { const e = JSON.parse(h); this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i) } catch (e) { const o = {}; this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i) } } else s = this.setval(t, e); return s } getval(t) { return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null } setval(t, e) { return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); s && this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } }).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) })) } post(t, e = (() => { })) { if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.post(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) }); else if (this.isQuanX()) t.method = "POST", this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t)); else if (this.isNode()) { this.initGotEnv(t); const { url: s, ...i } = t; this.got.post(s, i).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) }) } } time(t, e = null) { const s = e ? new Date(e) : new Date; let i = { "M+": s.getMonth() + 1, "d+": s.getDate(), "H+": s.getHours(), "m+": s.getMinutes(), "s+": s.getSeconds(), "q+": Math.floor((s.getMonth() + 3) / 3), S: s.getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, (s.getFullYear() + "").substr(4 - RegExp.$1.length))); for (let e in i) new RegExp("(" + e + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? i[e] : ("00" + i[e]).substr(("" + i[e]).length))); return t } msg(e = t, s = "", i = "", r) { const o = t => { if (!t) return t; if ("string" == typeof t) return this.isLoon() ? t : this.isQuanX() ? { "open-url": t } : this.isSurge() ? { url: t } : void 0; if ("object" == typeof t) { if (this.isLoon()) { let e = t.openUrl || t.url || t["open-url"], s = t.mediaUrl || t["media-url"]; return { openUrl: e, mediaUrl: s } } if (this.isQuanX()) { let e = t["open-url"] || t.url || t.openUrl, s = t["media-url"] || t.mediaUrl; return { "open-url": e, "media-url": s } } if (this.isSurge()) { let e = t.url || t.openUrl || t["open-url"]; return { url: e } } } }; if (this.isMute || (this.isSurge() || this.isLoon() ? $notification.post(e, s, i, o(r)) : this.isQuanX() && $notify(e, s, i, o(r))), !this.isMuteLog) { let t = ["", "==============📣系统通知📣=============="]; t.push(e), s && t.push(s), i && t.push(i), console.log(t.join("\n")), this.logs = this.logs.concat(t) } } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { const s = !this.isSurge() && !this.isQuanX() && !this.isLoon(); s ? this.log("", `❗️${this.name}, 错误!`, t.stack) : this.log("", `❗️${this.name}, 错误!`, t) } wait(t) { return new Promise(e => setTimeout(e, t)) } done(t = {}) { const e = (new Date).getTime(), s = (e - this.startTime) / 1e3; this.log("", `🔔${this.name}, 结束! 🕛 ${s} 秒`), this.log(), (this.isSurge() || this.isQuanX() || this.isLoon()) && $done(t) } }(t, e) }
