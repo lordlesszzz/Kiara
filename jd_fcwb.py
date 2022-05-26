ab7eca73eae53e2f8d92e466dad1dc31cdd1ac55
LordlessCamelot <3154536753@qq.com>
2022-05-26 15:30:31 +0800

    Update jd_fcwb.py

--- jd_fcwb.py
+++ jd_fcwb.py
@@ -382,8 +382,8 @@ def main():
     for e,cookie in enumerate(cookie_list,start=1):
         print(f'******开始【账号 {e}】 {get_pin(cookie)} *********\n')
         apDoTask(cookie)
-      #  happyDigHome(cookie)
-      #  spring_reward_list(cookie)
+        happyDigHome(cookie)
+        spring_reward_list(cookie)
 
 
 if __name__ == '__main__':
