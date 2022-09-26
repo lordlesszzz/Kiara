c75a4dae9fa3df992921e2a05c863203400767f3
LordlessCamelot <3154536753@qq.com>
2022-09-26 13:41:29 +0800

    更新

---
+++ sillyGirl/backup/jd_myselfUse.js
@@ -0,0 +1,12 @@
+/**
+* @author camelot
+* @rule t?
+* @public true
+* @admin true
+**/
+
+const s = sender
+
+let text = s.param(1) 
+let packetId=text.replace(/.*\&packetId\=([^\&]*)\&?.*/g,"$1")
+s.reply(`## 推一推极速\nexport tytpacketId="${packetId}"`)
