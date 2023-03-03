8ed241995ceb08325631e6be20e37f91cf06a036
LordlessCamelot <3154536753@qq.com>
2023-03-03 14:34:10 +0800

    Update jd_common_help.js

--- jd_common_help.js
+++ jd_common_help.js
@@ -7,7 +7,7 @@ cron 33 0 * * * jd_activity_help.js
 const $ = new Env('京东活动通用助力')
 const notify = $.isNode() ? require('./sendNotify') : ''
 //Node.js用户请在jdCookie.js处填写京东ck
-const jdCookieNode = $.isNode() ? require('../JD/jdCookie.js') : ''
+const jdCookieNode = $.isNode() ? require('.jdCookie.js') : ''
 CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = ''
